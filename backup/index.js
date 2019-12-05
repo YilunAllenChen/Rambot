4468686ffa34691c8279e871ed23504b761fa5bd
YilunAllenChen <allenchen@gatech.edu>
2019-12-05 11:32:21 +0800

    dev(): packager works

---
+++ backup/index.js
@@ -0,0 +1,82 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+
+const fs = require('fs')
+const express = require('express');
+var bodyParser = require("body-parser");
+var moment = require('moment');
+moment().format();
+
+// Node.js setup
+const hostname = 'localhost';
+const node = express();
+const port = 32123;
+node.set('port', process.env.PORT || port);
+node.use(bodyParser.urlencoded({
+  extended: false
+}));
+node.use(bodyParser.json());
+node.use(express.static(__dirname)); //so that the file of res.sendFile can source another file.
+//let the node listen.
+node.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+  fs.writeFileSync("port_info.config", 'http://'+hostname+':'+port);
+});
+
+
+fs.readFile("port_info.config", "utf8",function(err, data){
+  console.log(data);
+});
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      //preload: path.join(__dirname, 'preload.js')
+      webSecurity: false
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://'+__dirname+'/UI_build/index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
